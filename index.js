5f0dda47211c136e0bcb347816661d71d2d62b42
halfalpine <rocksandwoks@gmail.com>
2017-01-17 20:20:54 -0500

    Add soap and dwml packages

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ let express = require('express')
 let app = express()
 let noaaWeather = require('noaa-weather')
 let buffer = require('buffer')
+let soap = require('soap')
+
+var fs = require('fs');
+var parser = require('dwml-to-json');
+var xmlString = fs.readFileSync('path/to/dwml-file', 'utf8');
+var parsedData = dwmlParser.parse(xmlString)
 
 app.get('/', function(req, res){
   noaaWeather(40.586521, -73.795127)
@@ -12,20 +18,7 @@ app.get('/', function(req, res){
       res.send(err.toString())
     })
 })
-/*
-app.get('/', function(req,res) {
-  noaaWeather(40.586521, -73.795127)
-  .then(function(forecast){
-    res.json(forecast)
-    let buf = Buffer.from(forecast.body._readableState.buffer.head.data)
-    console.log(forecast.body._readableState.buffer.head.data.toString())
-    //let buf2 = Buffer.from(buf.data)
-    //console.log(JSON.stringify(buf))
 
-    //res.send(forecast)
-  })
-})
-*/
 app.listen(3000, function() {
   console.log("Serving")
 })
