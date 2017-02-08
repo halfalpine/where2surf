caeab6447d78f02828dacd532116d65ccfc052e3
Dirk Peters <rocksandwoks@gmail.com>
2017-02-08 13:53:16 -0500

    Add test fetch

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-app.use(require('./app/routes/index'));
+app.use(require('./app/js/index'));
 
 var server = app.listen(app.get('port'), function() {
   console.log(`Now serving on port ${port}`);
--- app/js/wave-predictor.js
+++ app/js/wave-predictor.js
@@ -1,8 +1,7 @@
 module.exports = function(forecast) {
 
-  return 'foo'
-  
   const current = forecast[1]
+
   if (curent.wind.direction >= 247.5 && current.wind.direction < 315) {
     return '67th St, Rockaway Beach'
   } else if (current.wind.direction >= 315 || current.wind.direction < 90) {
