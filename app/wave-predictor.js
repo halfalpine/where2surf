4182aa5dd845ed760c49d9c759e369584a7b18c3
halfalpine <rocksandwoks@gmail.com>
2017-02-07 19:45:16 -0500

    Initiate wave-predictor.js

---
+++ app/wave-predictor.js
@@ -0,0 +1,10 @@
+module.exports = function(forecast) {
+  const current = forecast[1]
+  if (curent.wind.direction >= 247.5 && current.wind.direction < 315) {
+    return '67th St, Rockaway Beach'
+  } else if (current.wind.direction >= 315 || current.wind.direction < 90) {
+    return 'Long Beach'
+  } else {
+    return 'If the winds are low, you may go.'
+  }
+}
