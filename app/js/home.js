8c83da319217b356a2e3dcab8a41dfbc76eb79f1
Dirk Peters <rocksandwoks@gmail.com>
2017-02-08 11:36:33 -0500

    Add consts to home.js

--- app/js/home.js
+++ app/js/home.js
@@ -1,12 +1,12 @@
 // The purpose of 'home' is to ping the server and
-require('dotenv').config()
+require('dotenv').config();
 
-const APIKEY = 
-const MSWURL =
+const APIKEY = process.env.MSW_KEY;
+const MSWURL = `http://magicseaweed.com/api/${APIKEY}/forecast/?spot_id=384`;
 
 if (self.fetch) {
   // make fetch requst
-  fetch()
+  fetch(MSWURL);
 } else {
   // some ajax voodoo
 }
