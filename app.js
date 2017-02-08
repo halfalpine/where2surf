fa45ffc1e8eabef1a907748685a54a0fe2ad9be7
Dirk Peters <rocksandwoks@gmail.com>
2017-02-08 13:42:26 -0500

    Add routes to app.js

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-app.get('/', function(){
-
-});
+app.use(require('./app/routes/index'));
 
 var server = app.listen(app.get('port'), function() {
   console.log(`Now serving on port ${port}`);
