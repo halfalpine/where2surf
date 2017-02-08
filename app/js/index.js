8266065b02f3c8f1eedd7cbb6dd317153951b4a1
Dirk Peters <rocksandwoks@gmail.com>
2017-02-08 17:00:09 -0500

    Clean up index.js

--- app/js/index.js
+++ app/js/index.js
@@ -1,13 +1,25 @@
 // The purpose of 'home' is to ping the server and
 require('dotenv').config();
+let express = require('express');
+let router = express.Router();
 let fetch = require('node-fetch');
 let wavePredictor = require('./wave-predictor.js');
 
-const API_KEY = process.env.MSW_KEY;
-const MSW_URL = `http://magicseaweed.com/api/${API_KEY}/forecast/?spot_id=384`;
 
-fetch(MSW_URL)
-.then(wavePredictor(response))
-.catch(function(err) {
-  console.err(err);
+router.get('/', function(req, res) {
+  const API_KEY = process.env.MSW_KEY;
+  const MSW_URL = `http://magicseaweed.com/api/${API_KEY}/forecast/?spot_id=384`;
+
+/*
+  fetch(MSW_URL)
+    .then(function(res) {
+      console.log(res);
+    })
+    .catch(function(err) {
+      console.err(err);
+    });
+*/
+    res.render('index');
 });
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 let express = require('express');
+let reload = require('reload');
 let app = express();
 
-let wavePredictor = require('./app/js/wave-predictor.js')
-
 app.set('port', process.env.PORT || 3000);
+console.log(process.env.PORT);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.use(require('./app/js/index'));
 
 var server = app.listen(app.get('port'), function() {
-  console.log(`Now serving on port ${port}`);
+  console.log(`Now serving on port ${app.get('port')}`);
 });
 
 reload(server, app);
