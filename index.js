bb3d8cc3e4b682628b49bcbfb90c15a747a2c028
halfalpine <rocksandwoks@gmail.com>
2017-02-07 22:09:12 -0500

    Remove extra packages

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 let express = require('express')
 let app = express()
-let noaaWeather = require('noaa-weather')
-let buffer = require('buffer')
 
 app.listen(3000, function() {
   console.log("Serving")
