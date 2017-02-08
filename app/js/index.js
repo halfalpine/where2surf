ee94e38e5588672faa05a765cb5d57617e7994ed
Dirk Peters <rocksandwoks@gmail.com>
2017-02-08 13:57:58 -0500

    Add node-fetch

--- app/js/index.js
+++ app/js/index.js
@@ -1,17 +1,13 @@
 // The purpose of 'home' is to ping the server and
 require('dotenv').config();
+let fetch = require('node-fetch');
 let wavePredictor = require('./wave-predictor.js');
 
 const API_KEY = process.env.MSW_KEY;
 const MSW_URL = `http://magicseaweed.com/api/${API_KEY}/forecast/?spot_id=384`;
 
-if (self.fetch) {
-  // make fetch requst
-  fetch(MSW_URL)
-    .then(wavePredictor(response))
-    .catch(function(err) {
-      console.err(err);
-    });
-} else {
-  console.log('Doesn\'t support fetch API!')
-}
+fetch(MSW_URL)
+.then(wavePredictor(response))
+.catch(function(err) {
+  console.err(err);
+});
