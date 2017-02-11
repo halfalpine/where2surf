7dadf83041382fe4e415c3fc815ce1a5d67c233f
halfalpine <rocksandwoks@gmail.com>
2017-02-10 21:06:35 -0500

    Create wave-predicor spec file

---
+++ spec/wave-predictor.js
@@ -0,0 +1,7 @@
+describe('It can predict the best surf spot', function() {
+  describe('It has basic fuctionality', function() {
+    it('Can test for true', function() {
+      expect(true).toBe(true);
+    })
+  });
+});
