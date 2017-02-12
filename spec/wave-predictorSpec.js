96992cb5e76ce2af235aea078357f37382c8d2d0
Dirk Peters <rocksandwoks@gmail.com>
2017-02-12 18:53:20 -0500

    Add dummy object to wave-predictor spec

--- spec/wave-predictorSpec.js
+++ spec/wave-predictorSpec.js
@@ -1,4 +1,34 @@
-var wavePredictor = require('../app/js/wave-predictor');
+let wavePredictor = require('../app/js/wave-predictor');
+let data = [
+  {},
+  {
+    swell: {
+      components: {
+        combined: {
+          height: null,
+          period: null
+        }
+      }
+    },
+    wind: {
+      speed: null,
+      direction: null
+    },
+    condition: {
+      temperature: null,
+      weather: null
+    }
+  }
+];
+
+json[1].swell.components.combined.height
+json[1].swell.components.combined.period
+json[1].wind.speed
+json[1].wind.direction
+json[1].condition.temperature
+json[1].condition.weather
+
+
 
 describe('It can predict the best surf spot', function() {
   describe('It has basic fuctionality', function() {
