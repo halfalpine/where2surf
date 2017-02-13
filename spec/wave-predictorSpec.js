c17728ed01e6ea11884393aa70e19d110eadd9db
halfalpine <rocksandwoks@gmail.com>
2017-02-13 09:20:47 -0500

    Correct numerous typos in wave-predictorSpec

--- spec/wave-predictorSpec.js
+++ spec/wave-predictorSpec.js
@@ -24,39 +24,31 @@ let data = [
 /*
 json[1].swell.components.combined.height
 json[1].swell.components.combined.period
-json[1].wind.speed
-json[1].wind.direction
+cjson[1].wind.direction
 json[1].condition.temperature
 json[1].condition.weather
 */
 
 
 describe('The wave-predictor module', function() {
-    it('can test for true', function() {
-      expect(true).toBe(true);
-    it('is a function', function() {
-      expect(typeof wave-predictor).toBe('function');
-    });
-    it('has a valid data set', function() {
-      expect(data).toBeDefined();
-    })
+  it('can test for true', function() {
+    expect(true).toBe(true);
   });
-  describe('It can acess the correct module', function() {
-    const sampleData = [
-      {
-        foo: 'foo'
-      },
-      {
-        wind: {
-          direction: 144
-        }
-      }
-    ];
-    it('Can access sample data', function() {
-      expect(sampleData).toBeTruthy(null);
-    });
-    it('Can use the right function', function() {
-      expect(wavePredictor(sampleData)).toEqual('If the winds are low, you may go.');
-    })
-  })
+  it('is a function', function() {
+    expect(wavePredictor).toEqual(jasmine.any(Function));
+  });
+  it('has a valid data set', function() {
+    expect(data).toBeDefined();
+  });
+  xit('returns a string', function() {
+    expect()
+  });
+});
+
+describe('The forecasting algorithm', function() {
+  it('works for strong south wind', function() {
+    data[1].wind.direction = 180;
+    data[1].wind.speed = 12;
+    expect(wavePredictor(data)).toEqual('Stay home.');
+  });
 });
