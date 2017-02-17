920595349145d625be815fd3992286956e87cfa3
halfalpine <rocksandwoks@gmail.com>
2017-02-16 19:37:34 -0500

    Delete prediction tests

--- spec/wave-predictorSpec.js
+++ spec/wave-predictorSpec.js
@@ -30,60 +30,16 @@ json[1].condition.weather
 */
 
 describe('The wave-predictor module', function() {
+
   it('can test for true', function() {
     expect(true).toBe(true);
   });
+  
   it('is a function', function() {
     expect(wavePredictor).toEqual(jasmine.any(Function));
   });
-  it('has a valid data set', function() {
-    expect(data).toBeDefined();
-  });
+
   it('returns a string', function() {
     expect(wavePredictor(data)).toEqual(jasmine.any(String));
   });
-});
-
-describe('The forecasting algorithm works for forecasts with', function() {
-
-  describe('a south wind,', function() {
-    data[1].wind.direction = 0;
-
-    it('blowing hard', function() {
-      data[1].wind.speed = 10;
-      expect(wavePredictor(data)).toEqual('Stay home');
-    });
-
-    describe('blowing gently,', function() {
-      data[1].wind.speed = 5;
-
-      describe('has very short-period swell', function() {
-        it('isn\'t good for surfing', function() {
-          data[1].swell.components.combined.period = 6;
-          expect(wavePredictor(data)).toEqual('Stay home');
-        });
-      });
-
-      describe('short-period swell,', function() {
-        data[1].swell.components.combined.period = 7;
-
-        it('has small waves', function() {
-          data[1].swell.components.combined.height = 2;
-          expect(wavePredictor(data)).toEqual('Stay home');
-        });
-
-        it('has medium waves', function() {
-          data[1].swell.components.combined.height = 3;
-          console.log('data', data[1].swell.components.combined);
-          expect(wavePredictor(data)).toEqual('Rockaway Beach');
-        });
-
-        xit('has large waves', function() {
-          data[1].swell.components.combined.height = 5;
-          expect(wavePredictor(data)).toEqual('Long Beach');
-        });
-      });
-    });
-  });
-
 });
