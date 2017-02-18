0f77401fe17f2aa453722092f0337c2cbf916fa9
Dirk Peters <rocksandwoks@gmail.com>
2017-02-18 10:52:04 -0500

    Add test for W wind

--- app/js/wave-predictor.js
+++ app/js/wave-predictor.js
@@ -6,6 +6,7 @@ module.exports = function(forecast) {
   const windSpeed = forecast[1].wind.speed;
 
 // Flow control for S wind
+  // In the MSW API, compass points are opposite (+/- 180)
   if (windDirection > 270 || windDirection < 90) {
     if (windSpeed < 10) {
       if (period > 6 && period < 9) {
@@ -23,6 +24,18 @@ module.exports = function(forecast) {
           return 'Long Beach';
         } else if (height >= 5) {
           return 'Jacob Riis';
+        }
+      }
+    }
+  }
+
+// Flow control for W wind
+  // In the MSW API, compass points are opposite (+/- 180)
+  if (windDirection > 247.5 - 180 || windDirection < 337.5 - 180) {
+    if (windSpeed < 10) {
+      if (period >= 7 && period < 9) {
+        if (height >= 3 && height < 4) {
+          return '';
         }
       }
     }
--- spec/wave-predictorSpec.js
+++ spec/wave-predictorSpec.js
@@ -166,4 +166,354 @@ describe('Wave predictor', function() {
 
     });
   });
+
+  describe('for west winds', function() {
+
+    describe('blowing softly', function() {
+
+      it('has a very short period', function() {
+        const data = apiHelper({
+          period: 6,
+          heght: 5,
+          direction: 90,
+          speed: 5
+        });
+        expect(wavePredictor(data)).toMatch(/Stay home/);
+      });
+
+      describe('short period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 2,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 3,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 5,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Long Beach/);
+        });
+      });
+
+      describe('medium period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 2,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 3,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 5,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Long Beach/);
+        });
+
+      });
+
+      describe('long period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 2,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 3,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Long Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 5,
+            direction: 90,
+            speed: 5
+          });
+          expect(wavePredictor(data)).toMatch(/Jacob Riis/);
+        });
+
+      });
+
+    });
+
+    describe('blowing moderately', function() {
+
+      it('has a very short period', function() {
+        const data = apiHelper({
+          period: 6,
+          heght: 5,
+          direction: 90,
+          speed: 12
+        });
+        expect(wavePredictor(data)).toMatch(/Stay home/);
+      });
+
+      describe('short period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 2,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 3,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 5,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+      });
+
+      describe('medium period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 2,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 3,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 5,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Long Beach/);
+        });
+
+      });
+
+      describe('long period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 2,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 3,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Long Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 5,
+            direction: 90,
+            speed: 12
+          });
+          expect(wavePredictor(data)).toMatch(/Jacob Riis/);
+        });
+
+      });
+
+    });
+
+    describe('blowing hard', function() {
+
+      it('has a very short period', function() {
+        const data = apiHelper({
+          period: 6,
+          heght: 5,
+          direction: 90,
+          speed: 18
+        });
+        expect(wavePredictor(data)).toMatch(/Stay home/);
+      });
+
+      describe('short period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 2,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 3,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 7,
+            height: 5,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+      });
+
+      describe('medium period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 2,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 3,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 9,
+            height: 5,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Long Beach/);
+        });
+
+      });
+
+      describe('long period', function() {
+
+        it('has small waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 2,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Stay home/);
+        });
+
+        it('has medium waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 3,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Rockaway Beach/);
+        });
+
+        it('has large waves', function() {
+          const data = apiHelper({
+            period: 11,
+            height: 5,
+            direction: 90,
+            speed: 18
+          });
+          expect(wavePredictor(data)).toMatch(/Jacob Riis/);
+        });
+
+      });
+
+    });
+
+  });
+
 });
