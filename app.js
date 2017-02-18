9291010849b4fd93125360c8d57722723767e3b8
Dirk Peters <rocksandwoks@gmail.com>
2017-02-18 13:59:31 -0500

    Remove reload package

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 let express = require('express');
-let reload = require('reload');
 let app = express();
 
 app.set('port', process.env.PORT || 3000);
@@ -12,5 +11,3 @@ app.use(require('./app/js/routers/index'));
 var server = app.listen(app.get('port'), function() {
   console.log(`Now serving on port ${app.get('port')}`);
 });
-
-reload(server, app);
