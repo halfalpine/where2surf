e38e1dc401f7863c504dad9393406569190fb63f
halfalpine <rocksandwoks@gmail.com>
2017-02-22 08:40:04 -0500

    Update name of env vars

--- app/js/lib/forecast.js
+++ app/js/lib/forecast.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 let fetch = require('node-fetch');
 let wavePredictor = require('./wave-predictor');
 
-const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.MSW_URL;
 const MSW_URL = `http://magicseaweed.com/api/${API_KEY}/forecast/?spot_id=384`;
 
 module.exports = fetch(MSW_URL)
    .then(res => res.json())
-   .then(data => wavePredictor(data))
+   .then(data => wavePredictor(data));
