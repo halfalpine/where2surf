d9d07bc46e45511a0c0ae8737392a0c64347aeda
Dirk Peters <rocksandwoks@gmail.com>
2017-03-04 16:51:06 -0500

    Wave-predictor returns forecast object

--- app/js/lib/wave-predictor.js
+++ app/js/lib/wave-predictor.js
@@ -1,7 +1,9 @@
 module.exports = function(forecast) {
+  let forecastObject = {};
   const { direction, speed } = forecast[1].wind;
   const { height, period } = forecast[1].swell.components.combined;
   // console.log({direction, speed, height, period});
+  let dest = 'Stay home';
 
 // Flow control for S wind
   // In the MSW API, compass points are opposite (+/- 180)
@@ -9,19 +11,19 @@ module.exports = function(forecast) {
     if (speed < 10) {
       if (period >= 7 && period < 9) {
         if (height >= 3) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         }
       } else if (period >= 9 && period < 11) {
         if (height >= 3 ) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         } else if (height >= 2) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         }
       } else if (period >= 11) {
         if (height > 1.5 && height < 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         } else if (height >= 5) {
-          return 'Jacob Riis';
+          dest = 'Jacob Riis';
         }
       }
     }
@@ -33,61 +35,61 @@ module.exports = function(forecast) {
     if (speed < 8) {
       if (period >= 7 && period < 9) {
         if (height >= 3 && height < 5) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         }
       } else if (period >= 9 && period < 11) {
         if (height >= 3 && height < 5) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         }
       } else if (period >= 11) {
         if (height >= 2 && height < 3) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 3 && height < 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         } else if (height >= 5) {
-          return 'Jacob Riis';
+          dest = 'Jacob Riis';
         }
       }
     } else if (speed >= 8 && speed < 16) {
       if (period >= 7 && period < 9) {
         if (height >= 3) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         }
       } else if (period >= 9 && period < 11) {
         if (height >= 3 && height < 5) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         }
       } else if (period >= 11) {
         if (height >= 2 && height < 3) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 3 && height < 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         } else if (height >= 5) {
-          return 'Jacob Riis';
+          dest = 'Jacob Riis';
         }
       }
     } else if (speed >= 18 && speed < 32) {
       if (period >= 7 && period < 9) {
         if (height >= 5) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         }
       } else if (period >= 9 && period < 11) {
         if (height >= 3 && height < 5) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 5) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         }
       } else if (period >= 11) {
         if (height >= 3 && height < 5) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 5) {
-          return 'Jacob Riis';
+          dest = 'Jacob Riis';
         }
       }
     }
@@ -97,25 +99,30 @@ module.exports = function(forecast) {
     // In the MSW API, compass points are opposite (+/- 180)
     if (direction >= 337.5 - 180 && direction < 90 + 180) {
       if (period >= 13 && height >= 4) {
-        return 'Democrat Point';
+        dest = 'Democrat Point';
       } else if (period >= 7 && period < 9) {
         if (height >= 3) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         }
       } else if (period >= 9 && period < 11) {
         if (height >= 2 && height < 3) {
-          return 'Rockaway Beach';
+          dest = 'Rockaway Beach';
         } else if (height >= 3) {
-          return 'Long Beach';
+          dest = 'Long Beach';
         }
       } else if (period >= 11) {
         if (height >= 5) {
-          return 'Jacob Riis';
+          dest = 'Jacob Riis';
         } else {
-          return 'Long Beach';
+          dest = 'Long Beach';
         }
       }
     }
 
-  return 'Stay home';
+  forecastObject.dest = dest;
+  forecastObject.direction = direction;
+  forecastObject.period = period;
+  forecastObject.speed = speed;
+  forecastObject.height = height;
+  return forecastObject;
 };
