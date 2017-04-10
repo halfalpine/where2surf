741deb10e889a214801b29b7dd5fab13883d99fb
Dirk Peters <rocksandwoks@gmail.com>
2017-04-10 18:52:27 -0400

    Change wind from degs to direction

--- middlewares/wave-predictor.js
+++ middlewares/wave-predictor.js
@@ -119,8 +119,40 @@ module.exports = function(forecast, index) {
       }
     }
 
+    function windInverter(direction){
+      direction += 180;
+      return direction >= 360 ? direction - 360 : direction;
+    }
+
+    function getCardinal(angle) {
+        //easy to customize by changing the number of directions you have
+        var directions = 8;
+
+        var degree = 360 / directions;
+        angle = angle + degree/2;
+
+        if (angle >= 0 * degree && angle < 1 * degree)
+            return 'N';
+        if (angle >= 1 * degree && angle < 2 * degree)
+            return 'NE';
+        if (angle >= 2 * degree && angle < 3 * degree)
+            return 'E';
+        if (angle >= 3 * degree && angle < 4 * degree)
+            return 'SE';
+        if (angle >= 4 * degree && angle < 5 * degree)
+            return 'S';
+        if (angle >= 5 * degree && angle < 6 * degree)
+            return 'SW';
+        if (angle >= 6 * degree && angle < 7 * degree)
+            return 'W';
+        if (angle >= 7 * degree && angle < 8 * degree)
+            return 'NW';
+        //Should never happen:
+        return 'N';
+    }
+
   forecastObject.dest = dest;
-  forecastObject.direction = direction;
+  forecastObject.direction = getCardinal(windInverter(direction));
   forecastObject.period = period;
   forecastObject.speed = speed;
   forecastObject.height = height;
